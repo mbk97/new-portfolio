9d41aacac123d7188bb260c74736fc4ea47a9e82
muhammed mubarak <oyindamola850@gmail.com>
2022-12-28 16:02:10 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ export default function Home() {
           name="keyword"
           content="portfolio, javascript, web developer, full-stack web developer, React js, Next Js, Styled-components, web development, front-end developer"
         />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:title"
           content="Mubarak | full-stack Web developer"
@@ -33,6 +34,11 @@ export default function Home() {
         <meta
           name="twitter:description"
           content="portfolio, javascript, web developer, full-stack web developer, React js, Next Js, Styled-components, web development, front-end developer"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content="blob:https://vercel.com/e1235895-eeff-40ed-88cd-ed324b3630d4"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
