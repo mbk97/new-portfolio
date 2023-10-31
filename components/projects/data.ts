6c5df47e90b790f5251832b8e547f84e204eafd3
mubarak.muhammed <mubarak.muhammed@wemabank.com>
2023-10-31 11:14:29 +0100

    update

--- components/projects/data.ts
+++ components/projects/data.ts
@@ -1,40 +1,40 @@
 export const projecData = [
-  {
-    id: 1,
-    name: "Heycard (Client)",
-    description:
-      "Discover Heycard, the game-changing spending management tool that brings automation and efficiency to expense tracking. Designed to streamline your business operations, Heycard empowers you to effortlessly monitor a range of crucial financial activities including subscriptions, payouts, transactions, and comprehensive spending reports. ",
-    projectImg: "/assets/img/hey-card.png",
-    technologies: [
-      { id: 1, img: "/assets/img/next.png", text: "Next JS" },
-      { id: 2, img: "/assets/img/tailwind.png", text: "Tailwind CSS" },
-      { id: 3, img: "/assets/img/redux.png", text: "Redux" },
-      { id: 4, img: "/assets/img/ant-design.png", text: "Ant Design" },
-      { id: 5, img: "/assets/img/formik.png", text: "Formik" },
-    ],
-    websiteLink: "https://heycard.app/",
-    aos: "flip-right",
-    class: true,
-  },
-  {
-    id: 2,
-    name: "Full-stack blog website",
-    description:
-      "A full-stack blog application built using the MERN stack along with Redux, Styled-components, and Material UI. This app allows authenticated users to Read, Create, Edit, and Delete their blog posts.",
-    projectImg: "/assets/img/blog.jpg",
-    technologies: [
-      { id: 1, img: "/assets/img/react.png", text: "React JS" },
-      { id: 2, img: "/assets/img/express.png", text: "Express" },
-      { id: 3, img: "/assets/img/mongo.png", text: "Mongo DB" },
-      { id: 4, img: "/assets/img/redux.png", text: "Redux" },
-      { id: 5, img: "/assets/img/ts.png", text: "Typescript" },
-      { id: 6, img: "/assets/img/styled.png", text: "Styled Components" },
-    ],
-    aos: "fade-up",
-    websiteLink: "https://blog-website-liart.vercel.app/",
-    githubLink: "https://github.com/mbk97/blog-website",
-    githubTwo: "https://github.com/mbk97/blog-website-api",
-  },
+  // {
+  //   id: 1,
+  //   name: "Heycard (Client)",
+  //   description:
+  //     "Discover Heycard, the game-changing spending management tool that brings automation and efficiency to expense tracking. Designed to streamline your business operations, Heycard empowers you to effortlessly monitor a range of crucial financial activities including subscriptions, payouts, transactions, and comprehensive spending reports. ",
+  //   projectImg: "/assets/img/hey-card.png",
+  //   technologies: [
+  //     { id: 1, img: "/assets/img/next.png", text: "Next JS" },
+  //     { id: 2, img: "/assets/img/tailwind.png", text: "Tailwind CSS" },
+  //     { id: 3, img: "/assets/img/redux.png", text: "Redux" },
+  //     { id: 4, img: "/assets/img/ant-design.png", text: "Ant Design" },
+  //     { id: 5, img: "/assets/img/formik.png", text: "Formik" },
+  //   ],
+  //   websiteLink: "https://heycard.app/",
+  //   aos: "flip-right",
+  //   class: true,
+  // },
+  // {
+  //   id: 2,
+  //   name: "Full-stack blog website",
+  //   description:
+  //     "A full-stack blog application built using the MERN stack along with Redux, Styled-components, and Material UI. This app allows authenticated users to Read, Create, Edit, and Delete their blog posts.",
+  //   projectImg: "/assets/img/blog.jpg",
+  //   technologies: [
+  //     { id: 1, img: "/assets/img/react.png", text: "React JS" },
+  //     { id: 2, img: "/assets/img/express.png", text: "Express" },
+  //     { id: 3, img: "/assets/img/mongo.png", text: "Mongo DB" },
+  //     { id: 4, img: "/assets/img/redux.png", text: "Redux" },
+  //     { id: 5, img: "/assets/img/ts.png", text: "Typescript" },
+  //     { id: 6, img: "/assets/img/styled.png", text: "Styled Components" },
+  //   ],
+  //   aos: "fade-up",
+  //   websiteLink: "https://blog-website-liart.vercel.app/",
+  //   githubLink: "https://github.com/mbk97/blog-website",
+  //   githubTwo: "https://github.com/mbk97/blog-website-api",
+  // },
   {
     id: 3,
     name: "BrandOn (Client)",
